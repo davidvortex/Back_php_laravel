67d72bb9899a6711b78382a82173f39c1200c63f
davidvortex <david_2428107@hotmail.com>
2025-03-20 11:06:22 -0600

    Crear While

---
+++ php/while.php
@@ -0,0 +1,20 @@
+<?php
+
+//la estructura while se ejecuta mientras la condicion sea verdadera
+//si la condicion es falsa, no se ejecuta
+//la estructura while es muy parecida a la estructura if
+//la diferencia es que if se ejecuta una sola vez y while se ejecuta varias veces
+//la estructura while se usa cuando no sabemos cuantas veces se va a ejecutar el codigo
+
+$i = 0;
+
+while ($i < 10) {
+    echo "El valor de i es: $i <br>";
+    $i++;
+}
+
+
+
+
+
+

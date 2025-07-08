083408b724b8669c53d28ca477e3773628c1a41d
Davidvortex <david@hotmail.com>
2025-07-08 15:47:13 -0600

    Crear rama dev

---
+++ Introduccion a PHP/variables.php
@@ -0,0 +1,13 @@
+<?php
+
+$a = 1;
+$A = 2;
+$nombre = "Juan";
+
+
+echo $a . " "; // Imprime 1
+echo $A; // Imprime 2
+echo $nombre; // Imprime Juan
+echo "<br>";
+$nombre = "Pedro";
+echo $nombre; // Imprime Pedro

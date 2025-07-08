66f35c638c11e1f601bb99e7bb4e36b7f2950fb6
Davidvortex <david@hotmail.com>
2025-07-08 17:53:46 -0600

    Terminar Introduccion a PHP

---
+++ Introduccion a PHP/ForEach.php
@@ -0,0 +1,15 @@
+<?php
+
+$name = [
+    "David",
+    "Juan",
+    "Pedro",
+    "Maria"
+];
+
+foreach ($name as $value) {
+    echo "El nombre es: $value <br>";
+}
+
+// El bucle foreach se utiliza para recorrer arrays en PHP
+// y es especialmente útil cuando se trabaja con arrays asociativos.
